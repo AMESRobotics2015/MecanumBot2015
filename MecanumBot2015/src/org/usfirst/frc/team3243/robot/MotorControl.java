8a3b23be3a0585a90e3ae520ddecf14c92acafd5
AMESRobotics2015 <ames.amperes@gmail.com>
2015-01-24 11:52:55 -0700

    Update to prepare for driving

--- MecanumBot2015/src/org/usfirst/frc/team3243/robot/MotorControl.java
+++ MecanumBot2015/src/org/usfirst/frc/team3243/robot/MotorControl.java
@@ -7,17 +7,19 @@ public class MotorControl {
 	//Create Motor objects here.
 	protected static RobotDrive drv;
 	private static Sensors S;
+	protected static double angle;
 	
 	public MotorControl(){
 		
 		drv = new RobotDrive(0,1,2,3);
 		S = new Sensors();
+		angle = S.readgy();
 		
 	}
 	
 	public void DriveMec(double[] axis){
 	
-		drv.mecanumDrive_Cartesian(axis[1], axis[0], axis[2], S.readgy());
+		drv.mecanumDrive_Cartesian(axis[1], axis[0], axis[2], 0);
 		
 	}
 
