bbb483b3ed9bc75132f754caff6653774bf228e9
AMESRobotics2015 <ames.amperes@gmail.com>
2015-02-05 16:47:42 -0800

    Changed the way the motors work

--- MecanumBot2015/src/org/usfirst/frc/team3243/robot/MotorControl.java
+++ MecanumBot2015/src/org/usfirst/frc/team3243/robot/MotorControl.java
@@ -5,15 +5,22 @@ import edu.wpi.first.wpilibj.*;
 public class MotorControl {
 	
 	//Create Motor objects here.
-	protected static RobotDrive drv;//creates an instance of the frc class RobotDrive called drv
+	//protected static RobotDrive drv;//creates an instance of the frc class RobotDrive called drv
 	private static Sensors S;//instance of the class Sensors
 	protected static double angle;//angle
 	protected static Victor elevator;//motor in charge of elevator
-	protected static Solenoid solenoid1, solenoid2;//solenoid motors
+	protected static  Solenoid solenoid1, solenoid2;//solenoid motors
+	protected static Talon topleft, topright, bottomleft, bottomright;
+	
+	static double[] drv= new double[4];
 	
 	public MotorControl(){
 		
-		drv = new RobotDrive(0,1,2,3);//constructor 
+		//drv = new RobotDrive(0,1,2,3);//constructor 
+		topleft = new Talon(0);
+		topright = new Talon(1);
+		bottomleft = new Talon(2);
+		bottomright = new Talon(3);
 		S = new Sensors();//instance of sensors is created
 		angle = S.readgy();//calls upon the lass readgy which is located in sensors
 		elevator = new Victor(4);
@@ -23,9 +30,24 @@ public class MotorControl {
 	}
 	
 	public void DriveMec(double[] axis){
-	
-		drv.mecanumDrive_Cartesian(axis[1], axis[0], axis[2], S.readgy());//frc class to allow driving
+		
+		finaldrv(driv);
+		
+		topleft.set(drv[0]);
+		topright.set(drv[1]);
+		bottomleft.set(drv[2]);
+		bottomright.set(drv[3]);
+		//drv.mecanumDrive_Cartesian(axis[1], axis[0], axis[2], S.readgy());//frc class to allow driving
 		System.out.println(angle);//to see if the gyro works
+		
+	}
+	public double[] finaldrv(double[] driv){
+		
+		drv[0] = (driv[0] * .75) - (driv[1] * .75) + (driv[2]);
+		drv[1] = ((driv[0] * .75) + driv[1] * .75 + (driv[2])) * .96;
+		drv[2] = -(driv[0] * .75) + (driv[1] * .75) + (driv[2]);
+		drv[3] = (-(driv[0] * .75) - (driv[1] * .75) + (driv[2])) * .96;
+		return drv;
 		
 	}
 	public void getGrabberMethod(double[] solenoidInput){//if open is pressed
