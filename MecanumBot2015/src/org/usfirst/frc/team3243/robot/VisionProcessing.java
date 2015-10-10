a741d9ed2e161a2886727727153a5434b037754e
Mark Van der Merwe <vandermerwem2@gmail.com>
2015-10-10 14:22:28 -0600

    Added new class for VisionProcessing.
    Started messing around with some of the functions. Not sure if correct
    but just getting started. Commented out to keep code functional.

---
+++ MecanumBot2015/src/org/usfirst/frc/team3243/robot/VisionProcessing.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team3243.robot;
+
+import javax.xml.bind.ValidationEventHandler;
+
+import com.ni.vision.NIVision.*;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.image.*;
+import edu.wpi.first.wpilibj.vision.*;
+
+public class VisionProcessing {
+/*
+	AxisCamera cam = new AxisCamera(in);
+	MonoImage m = new MonoImage();
+	Image image;
+	
+	public void image(){
+		cam.getImage(image);
+		
+	}
+	*/
+}
