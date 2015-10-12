2a8b4c5d0a1c70bbde80e07d9d27d716555e5fa4
Mark Van der Merwe <vandermerwem2@gmail.com>
2015-10-12 13:46:39 -0600

    Quick update for Tarun.

--- MecanumBot2015/src/org/usfirst/frc/team3243/robot/VisionProcessing.java
+++ MecanumBot2015/src/org/usfirst/frc/team3243/robot/VisionProcessing.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team3243.robot;
 
-import javax.xml.bind.ValidationEventHandler;
-
 import com.ni.vision.NIVision.*;
-
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.image.*;
 import edu.wpi.first.wpilibj.vision.*;
